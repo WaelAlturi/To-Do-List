36584f9827b3a6c9267afbce51ab6364026efe06
WaelAlturi <alturiwael@gmail.com>
2025-04-28 17:36:08 +0300

    done with add new task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,45 @@ import {
   TrashIcon,
 } from "@heroicons/react/24/solid";
 export default function App() {
+<<<<<<< HEAD
+=======
+  const [data, setdata] = useState([]);
+  const [newtask, setnewtask] = useState("");
+  const tasks = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/");
+      setdata(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const newTask = async () => {
+    try {
+      await axios.post("http://localhost:3000/newtask", {
+        newtask,
+      });
+      setdata((prevData) => [
+        ...prevData,
+        { _id: Date.now().toString(), task: newtask }, // creating a new task object with a unique id handel it as key for ul
+      ]);
+      setnewtask("");
+      console.log("New Task Added");
+    } catch (error) {
+      console.log("Error adding task:", error.message);
+    }
+  };
+
+  const deleteTask = () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    tasks();
+  }, []);
+>>>>>>> 0d2e99f (done with add new task)
   return (
     // <main className="app-container">
     //   {/* Main Layout Wrapper */}
@@ -36,11 +75,19 @@ export default function App() {
             <PlusIcon className="h-2/4 w-1/4  rounded-3xl transition scale-125 border-2 ease-in-out duration-1000 hover:rotate-90 hover:scale-150 hover:bg-black hover:text-amber-50" />
           </nav>
         </header>
+<<<<<<< HEAD
         <section className="h-full w-full text-lg font-bold bg-amber-100 pt-5">
           <ul className="h-full w-full flex p-2 justify-between">
             <li
               onClick={(e) => e.currentTarget.classList.toggle("line-through")}
               className="w-4/5 h-1/12 transition cursor-pointer  ease-in-out duration-1000   "
+=======
+        <section className="h-full w-full text-lg font-bold bg-amber-100 pt-2 overflow-auto">
+          {data.map((item) => (
+            <ul
+              key={item._id}
+              className="h-1/12 w-full flex p-2 justify-between"
+>>>>>>> 0d2e99f (done with add new task)
             >
               Customized
             </li>
